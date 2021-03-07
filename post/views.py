a328dc80d6a3e3e3d006003ae1132d003f5654bc
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-07 19:12:24 +0330

    When the user submits his post, refer to the article list page

--- post/views.py
+++ post/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from . import models
 from django.contrib.auth.decorators import login_required
 from . import forms
-
+from articles import urls
 # Create your views here.
 
 
 @login_required(login_url="/accounts/login")
 def add_post(request):
-    form = forms.Create_post()
+    if request.method == 'POST':
+        form = forms.Create_post(request.POST)
+        if form.is_valid():
+            return redirect('articles:list')
+    else:
+        form = forms.Create_post()
     return render(request, 'post_visit/add_post.html', {'form': form})
