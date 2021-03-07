9c4e50f83afb4152b87caf6f542f0f907f4060da
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-07 18:38:10 +0330

    Correction {'form':form}

--- post/views.py
+++ post/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from . import models
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
 
 @login_required(login_url="/accounts/login")
 def add_post(request):
-    return render(request, 'post_visit/add_post.html', {'post': add_post})
+    form = forms.Create_post()
+    return render(request, 'post_visit/add_post.html', {'form': form})
