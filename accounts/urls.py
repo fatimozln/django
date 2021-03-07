bed8dc036a85d78aa3393dcbca0c4c02841aca48
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-07 15:08:41 +0330

    add new path for logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 
-app_name = 'acconts'
+app_name = 'accounts'
 urlpatterns = [
     path('signup', views.signup_view, name='signup'),
     path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
 ]
