5ef6336dbc4643d8eb2481aed9a88311d43f7f82
Fateme Zalnezhad <fatemezalnezhad1999@gmail.com>
2021-03-07 18:33:51 +0330

    add date to class

--- post/models.py
+++ post/models.py
@@ -6,10 +6,11 @@ from django.urls import reverse
 # Create your models here.
 
 
-class post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
